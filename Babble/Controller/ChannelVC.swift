24cc24d72a212d83deff0924904e8b51e41e61d8
Pavan Kumar N <pavankumar.n.46@gmail.com>
2018-11-19 14:16:39 +0530

    add pan and touch gestures to SWRevealViewController. also, change the width of rearViewRevealWidth to its view size - 60

--- Babble/Controller/ChannelVC.swift
+++ Babble/Controller/ChannelVC.swift
@@ -12,19 +12,10 @@ class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if self.revealViewController() != nil{
+            self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
