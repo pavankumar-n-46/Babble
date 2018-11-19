1dba8e7ef470ea0bc617a4c3a34b3ae54b8393b6
Pavan Kumar N <pavankumar.n.46@gmail.com>
2018-11-19 22:06:19 +0530

    add GradientView. get the channel view UI implementation done.

---
+++ Babble/View/GradientView.swift
@@ -0,0 +1,36 @@
+//
+//  GradientView.swift
+//  Babble
+//
+//  Created by Pavan Kumar N on 19/11/2018.
+//  Copyright © 2018 Pavan Kumar N. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+   
+    @IBInspectable var topColor : UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1){
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor : UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1){
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer : CAGradientLayer = CAGradientLayer()
+        /// if you miss out to add .cgColot then this won't work, I scratched my head so much for this mistake.
+        gradientLayer.colors = [topColor.cgColor,bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+}
