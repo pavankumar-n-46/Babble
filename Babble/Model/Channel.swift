13eec685a5fa7470450918fe9550e441b5814452
Pavan Kumar N <pavankumar.n.46@gmail.com>
2018-12-11 13:54:10 +0530

    [feature] - get channel list
    add MessageService which for now will return all the channels and Channel struct which is used to store channel details.

---
+++ Babble/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Babble
+//
+//  Created by Pavan Kumar N on 11/12/2018.
+//  Copyright © 2018 Pavan Kumar N. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle : String!
+    public private(set) var channelDescription: String!
+    public private(set) var id : String!
+}
---
+++ Babble/Services/MessageService.swift
@@ -0,0 +1,49 @@
+//
+//  MessageService.swift
+//  Babble
+//
+//  Created by Pavan Kumar N on 11/12/2018.
+//  Copyright © 2018 Pavan Kumar N. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannels(completion: @escaping ((Bool)->Void)){
+        
+        request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: HEADER_AUTH).responseJSON { (response) in
+            if response.result.error == nil{
+                guard let data = response.data else {return}
+                //using json decoder
+//                do{
+//                    self.channels = try JSONDecoder().decode([Channel].self, from: data)
+//                } catch let error {
+//                    debugPrint(error as Any)
+//                }
+                
+                //using swiftyJSON
+                if let json = JSON(data: data).array{
+                    for index in json{
+                        let name = index["name"].stringValue
+                        let channelDescription = index["description"].stringValue
+                        let id = index["_id"].stringValue
+                        let channel = Channel.init(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+                    completion(true)
+                }
+            }else{
+                debugPrint(response.result.error as Any)
+                completion(false)
+            }
+        }
+    }
+    
+}
